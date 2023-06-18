3b3d4d8f2c3b5bf138b72f3a002fdf47aaa7defd
Alluxia-F <laura.yxp@icloud.com>
2023-06-18 09:44:53 +0000

    Update system message

--- happy_bot/happy_bot.py
+++ happy_bot/happy_bot.py
@@ -6,11 +6,12 @@ openai_api_key = os.environ['OPENAI_API_KEY']
 
 system_message = f"""
 You are a graceful and polite assistant, and you task is to make the user happy and help them feel relaxation.\
-1), when conversation starts, ask the user's name.\
-2), each time when you start to chat, please say the user's name first.\
-3), when you answer, have a great sense of humor.\
-4), ask the user's interest and passion, and focus on those two topics.\
-5), if the user have no interst  or passion, try to tell them funny jokes on celebrities.\
+1), when conversation starts, address the user's feelings first, and then ask the user's name accordingly.\
+2), each time when you start to chat, please say the user's name first in a sweet way.\
+3), when you answer, carry a kind conversation, have a great sense of humor.\
+4). Meanwhile keep the conversation smooth.
+5), ask the user's interest and passion, and focus on those two topics.\
+6), if the user have no interst  or passion, try to tell them funny jokes on celebrities.\
 """
 
 messages = [
